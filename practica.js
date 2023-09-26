48f2561ac38e3373dc0c44c97971c84d825b1028
a20150084 <131173253+a20150084@users.noreply.github.com>
2023-09-26 08:43:41 -0500

    Add files via upload

---
+++ practica.js
@@ -0,0 +1,7 @@
+var nom=prompt("Ingrese su nombre");
+document.write(" Hola " , nom );
+var preg1=prompt("¿Cuándo se realizará la inauguración de las Olimpiadas?\nA. Lunes 02 de octubre\nB. Viernes 29 de setiembre\nC. Jueves 28 de setiembre");
+if (preg1==B){
+    (preg1=1)
+}
+
